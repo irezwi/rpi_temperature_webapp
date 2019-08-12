cbad85c6334f91bde446457c274c2dd46b4edc64
irezwi <zwirek.ireneusz@gmail.com>
2019-08-12 22:59:40 +0200

    Implement last day chart

--- rpi_temperature_webapp/extras/TemperatureChartData.py
+++ rpi_temperature_webapp/extras/TemperatureChartData.py
@@ -7,8 +7,21 @@ class TemperatureChartData:
     def __init__(self):
         self.values = []
         self.labels = []
+        self.units = "C"
 
     def fill_with_data(self, start_date, end_date):
         for measurement in Measurements.objects.complex_filter(Q(time__gte=start_date, time__lte=end_date)):
             self.labels.append(measurement.time.strftime("%H:%M"))
             self.values.append(measurement.temperature)
+
+    def scale_temperatures_to_K(self):
+        if self.units == "C":
+            self.values = [value + 273.15 for value in self.values]
+        elif self.units == "K":
+            pass
+
+    def scale_temperatures_to_C(self):
+        if self.units == "C":
+            pass
+        elif self.units == "K":
+            self.values = [value - 273.15 for value in self.values]
--- rpi_temperature_webapp/views.py
+++ rpi_temperature_webapp/views.py
@@ -10,9 +10,14 @@ from rpi_temperature_webapp.extras.TemperatureChartData import TemperatureChartD
 class HomeView(View):
     def get(self, request, *args, **kwargs):
         one_hour_ago = timezone.localtime(timezone.now() - timedelta(hours=1))
+        one_day_ago = timezone.localtime(timezone.now() - timedelta(days=1))
         now = timezone.localtime()
 
         last_hour_chart_data = TemperatureChartData()
         last_hour_chart_data.fill_with_data(one_hour_ago, now)
 
-        return render(request, 'charts.html', {"lastHourChartData": last_hour_chart_data})
+        last_day_chart_data = TemperatureChartData()
+        last_day_chart_data.fill_with_data(one_day_ago, now)
+
+        return render(request, 'charts.html', {"lastHourChartData": last_hour_chart_data,
+                                               "lastDayChartData": last_day_chart_data})
