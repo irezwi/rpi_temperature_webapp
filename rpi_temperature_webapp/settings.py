a875d7c329509eedbe873aebc70ad2f244e92461
irezwi <zwirek.ireneusz@gmail.com>
2019-08-12 19:52:41 +0200

    Update allowed hosts

--- rpi_temperature_webapp/settings.py
+++ rpi_temperature_webapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['192.168.0.101', '127.0.0.1']
+ALLOWED_HOSTS = ['192.168.0.101', '127.0.0.1', 'rpi-temperature.herokuapp.com']
 
 
 # Application definition
