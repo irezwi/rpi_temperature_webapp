32cf24af36d3d89dd9e068aaa09a36a793b4ae8d
irezwi <zwirek.ireneusz@gmail.com>
2019-08-23 15:01:48 +0200

    Modify number of points on 24h chart

--- rpi_temperature_webapp/extras/TemperatureChartData.py
+++ rpi_temperature_webapp/extras/TemperatureChartData.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 from django.db.models import Q
 
 from rpi_temperature_webapp.models import Measurements
@@ -10,9 +12,15 @@ class TemperatureChartData:
         self.units = "C"
 
     def fill_with_data(self, start_date, end_date):
-        for measurement in Measurements.objects.complex_filter(Q(time__gte=start_date, time__lte=end_date)):
+        measurements = Measurements.objects.complex_filter(Q(time__gte=start_date, time__lte=end_date))
+
+        for measurement in measurements:
             self.labels.append(measurement.time.strftime("%H:%M"))
             self.values.append(measurement.temperature)
+
+        if len(measurements) > 100:
+            self.labels = self.labels[::10]
+            self.values = self.values[::10]
 
     def scale_temperatures_to_K(self):
         if self.units == "C":
